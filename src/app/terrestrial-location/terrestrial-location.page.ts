47bee62e68d83c98e69222deb8645fd75a8ca529
Joe B <Joe@bcjj.com>
2025-01-31 14:30:35 -0500

    google map added

--- src/app/terrestrial-location/terrestrial-location.page.ts
+++ src/app/terrestrial-location/terrestrial-location.page.ts
@@ -1,23 +1,38 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-terrestrial-location',
-  templateUrl: './terrestrial-location.page.html',
-  styleUrls: ['./terrestrial-location.page.scss'],
+  template: `
+    <ion-content>
+      <google-map height="100vh" width="100%"
+        [center]="center"
+        [zoom]="zoom">
+        <map-marker [position]="center"></map-marker>
+      </google-map>
+    </ion-content>
+  `,
+  styles: [`
+    google-map {
+      display: block;
+      width: 100%;
+      height: 100vh;
+    }
+  `],
   standalone: true,
-  imports: [
-    CommonModule,
-    IonicModule,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    ExploreContainerComponent]
+  imports: [IonContent, GoogleMapsModule]
 })
-export class TerrestrialLocationPage {
-  constructor() { }
+export class TerrestrialLocationPage implements OnInit {
+  center: google.maps.LatLngLiteral = {lat: 0, lng: 0};
+  zoom = 15;
+
+  async ngOnInit() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.center = {
+      lat: coordinates.coords.latitude,
+      lng: coordinates.coords.longitude
+    };
+  }
 }
